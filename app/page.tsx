0f2344d083b533eb20923936658ca8a083807b67
hakimxiao <alhakimabusolihin@gmail.com>
2026-01-10 22:21:58 +0700

    mengimplementasikan navigation bar dengan component Header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <p className="text-3xl text-indigo-500">CoinPulse</p>
+        <p className="text-3xl text-indigo-500"></p>
     )
 }
 export default Page
