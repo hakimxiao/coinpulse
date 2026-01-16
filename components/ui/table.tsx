accb9b733cdc85a350b1453383968eb1b54df1fb
hakimxiao <alhakimabusolihin@gmail.com>
2026-01-16 22:51:39 +0700

    fixes coderabbit issues

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -4,43 +4,53 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-function Table({ className, ...props }: React.ComponentProps<'table'>) {
+function Table({ className, children, ...props }: React.ComponentProps<'table'>) {
   return (
     <div data-slot="table-container" className="relative w-full overflow-x-auto">
       <table
         data-slot="table"
         className={cn('w-full caption-bottom text-sm', className)}
         {...props}
-      />
+      >
+        {children}
+      </table>
     </div>
   );
 }
 
-function TableHeader({ className, ...props }: React.ComponentProps<'thead'>) {
-  return <thead data-slot="table-header" className={cn('[&_tr]:border-b', className)} {...props} />;
+function TableHeader({ className, children, ...props }: React.ComponentProps<'thead'>) {
+  return (
+    <thead data-slot="table-header" className={cn('[&_tr]:border-b', className)} {...props}>
+      {children}
+    </thead>
+  );
 }
 
-function TableBody({ className, ...props }: React.ComponentProps<'tbody'>) {
+function TableBody({ className, children, ...props }: React.ComponentProps<'tbody'>) {
   return (
     <tbody
       data-slot="table-body"
       className={cn('[&_tr:last-child]:border-0', className)}
       {...props}
-    />
+    >
+      {children}
+    </tbody>
   );
 }
 
-function TableFooter({ className, ...props }: React.ComponentProps<'tfoot'>) {
+function TableFooter({ className, children, ...props }: React.ComponentProps<'tfoot'>) {
   return (
     <tfoot
       data-slot="table-footer"
       className={cn('bg-muted/50 border-t font-medium [&>tr]:last:border-b-0', className)}
       {...props}
-    />
+    >
+      {children}
+    </tfoot>
   );
 }
 
-function TableRow({ className, ...props }: React.ComponentProps<'tr'>) {
+function TableRow({ className, children, ...props }: React.ComponentProps<'tr'>) {
   return (
     <tr
       data-slot="table-row"
@@ -49,11 +59,13 @@ function TableRow({ className, ...props }: React.ComponentProps<'tr'>) {
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </tr>
   );
 }
 
-function TableHead({ className, ...props }: React.ComponentProps<'th'>) {
+function TableHead({ className, children, ...props }: React.ComponentProps<'th'>) {
   return (
     <th
       data-slot="table-head"
@@ -62,11 +74,13 @@ function TableHead({ className, ...props }: React.ComponentProps<'th'>) {
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </th>
   );
 }
 
-function TableCell({ className, ...props }: React.ComponentProps<'td'>) {
+function TableCell({ className, children, ...props }: React.ComponentProps<'td'>) {
   return (
     <td
       data-slot="table-cell"
@@ -75,17 +89,21 @@ function TableCell({ className, ...props }: React.ComponentProps<'td'>) {
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </td>
   );
 }
 
-function TableCaption({ className, ...props }: React.ComponentProps<'caption'>) {
+function TableCaption({ className, children, ...props }: React.ComponentProps<'caption'>) {
   return (
     <caption
       data-slot="table-caption"
       className={cn('text-muted-foreground mt-4 text-sm', className)}
       {...props}
-    />
+    >
+      {children}
+    </caption>
   );
 }
 
