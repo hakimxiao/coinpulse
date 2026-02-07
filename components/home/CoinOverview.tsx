2dc0aab06dc00d5709b99fad646a7123ecd48eff
hakimxiao <alhakimabusolihin@gmail.com>
2026-02-07 23:49:39 +0700

    implement candle stick chart

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,27 +1,43 @@
 import { fetcher } from '@/lib/coingecko.action';
 import { formatIDR, formatUSD } from '@/lib/utils';
 import Image from 'next/image';
+import { CoinOverviewFallback } from '@/components/home/fallback';
+import CandleStickChart from '@/components/CandleStickChart';
 
 const CoinOverview = async () => {
-  const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
-    dex_pair_format: 'symbol',
-  });
+  try {
+    const [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>('/coins/bitcoin', {
+        dex_pair_format: 'symbol',
+      }),
+      fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
+        vs_currency: 'usd',
+        days: 1,
+        precision: 'full',
+      }),
+    ]);
 
-  return (
-    <div id="coin-overview">
-      <div className="header pt-2">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol.toUpperCase()}
-          </p>
-          <h1>
-            {formatUSD(coin.market_data.current_price.usd)} /{' '}
-            {formatIDR(coin.market_data.current_price.usd)}{' '}
-          </h1>
-        </div>
+    return (
+      <div id="coin-overview">
+        <CandleStickChart data={coinOHLCData} coinId="bitcoin">
+          <div className="header pt-2">
+            <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
+            <div className="info">
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>
+                {formatUSD(coin.market_data.current_price.usd)} /{' '}
+                {formatIDR(coin.market_data.current_price.usd)}{' '}
+              </h1>
+            </div>
+          </div>
+        </CandleStickChart>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error('Error fetching coin overview:', error);
+    return <CoinOverviewFallback />;
+  }
 };
 export default CoinOverview;
